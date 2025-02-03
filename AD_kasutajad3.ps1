da24c3e3c96caf06951720ee01e2a28aab685b2e
Aleksander Püüa <aleksander.puua@voco.ee>
2025-02-03 11:39:24 +0200

    AD kasutaja loomine failist

---
+++ AD_kasutajad3.ps1
@@ -0,0 +1,10 @@
+﻿$Fail = "C:\Users\Administrator\AD kasutajad\kasutajad.csv"
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+foreach ($Kasutaja in $Kasutajad)
+{
+$Kasutajanimi = $Kasutaja.Kasutajanimi
+$Taisnimi = $Kasutaja.Taisnimi
+$KontoKirjeldus = $kasutaja.KontoKirjeldus
+$Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+New-LocalUser -Name $Kasutajanimi -Password $Parool -FullName "$Taisnimi" -Description "$KontoKirjeldus"
+}
